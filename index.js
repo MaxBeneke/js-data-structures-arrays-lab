31c89cb6b2f2ef3c011f17ce41835c5f38726ecc
Max Beneke <max.beneke@gmail.com>
2021-02-20 15:18:02 -0500

    commit tree 214622e89b50fc21235378bc515af0ea0d932795

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+};
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+};
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+};
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+};
+
+function appendDriver(name) {
+    drivers.push(name);
+};
+
+function prependDriver(name) {
+   return drivers.splice(0, 0, name);
+};
+
+function removeLastDriver() {
+    console.log(drivers.slice(-1));
+};
+
+function removeFirstDriver() {
+    drivers.slice(1);
+};
